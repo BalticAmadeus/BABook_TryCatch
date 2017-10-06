ff2ec24cb0381bcb924e31657d1620375e3c8b3c
Gabrielius Kazakevičius <gkazakevic@gmail.com>
2017-10-06 14:14:27 +0300

    Initial

---
+++ BaBook_Backend/BaBook_Backend/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BaBook_Backend.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ BaBook_Backend/BaBook_Backend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BaBook_Backend.WebApiApplication" Language="C#" %>
