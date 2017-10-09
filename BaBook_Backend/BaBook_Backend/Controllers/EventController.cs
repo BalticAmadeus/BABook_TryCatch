09fce0206bbbe1275750183e323a59652f4eebb0
Gabrielius Kazakevičius <gkazakevic@gmail.com>
2017-10-09 12:14:11 +0300

    First working api requests

--- BaBook_Backend/BaBook_Backend/Controllers/EventController.cs
+++ BaBook_Backend/BaBook_Backend/Controllers/EventController.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
-using System.Web.Mvc;
 using BaBook_Backend.Mapper;
+using BaBook_Backend.Models;
 using BaBook_Backend.Repositories;
 using BaBook_Backend.ViewModels;
 
@@ -12,11 +12,28 @@ namespace BaBook_Backend.Controllers
 {
     public class EventController : ApiController
     {
-        public IHttpActionResult CreateEvent(EventViewModel model)
+
+        [Route("api/events")]
+        public IHttpActionResult GetEvents()
         {
-            using (var _repository = new EventRepository())
+            using (var repository = new EventRepository())
             {
-                _repository.Add(ViewModelToDomainMapping.MapEvent(model));
+                var toReturn = new List<EventViewModel>();
+                var models = repository.Get();
+                models.ForEach(x => toReturn.Add(DomainToViewModelMapping.MapEventViewModel(x)));
+                
+                return Ok(toReturn);
+            }
+        }
+
+        [HttpPut]
+        [Route("api/events")]
+        public IHttpActionResult CreateEvent(CreateEventViewModel model)
+        {
+            using (var repository = new EventRepository())
+            {
+                var newEvent = ViewModelToDomainMapping.MapEvent(model);
+                repository.Add(newEvent, model.OwnerId, model.GroupId);
                 return Ok();
             }
         }
---
+++ BaBook_Backend/BaBook_Backend/Migrations/201710090850157_test.cs
@@ -0,0 +1,16 @@
+namespace BaBook_Backend.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class test : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
