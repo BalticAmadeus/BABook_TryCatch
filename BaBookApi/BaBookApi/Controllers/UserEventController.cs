b29b47c977631e2dc96b4cca06e296779a3ed878
Andzej Korovacki <Andrzej.korovacki@gmail.com>
2017-10-10 12:19:30 +0300

    Add_User_To_Event

---
+++ BaBookApi/BaBookApi/Controllers/UserEventController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Domain.Models;
+using DataAccess.Repositories;
+
+namespace BaBookApi.Controllers
+{
+    public class UserEventController : ApiController
+    {
+        [AllowAnonymous]
+        [Route("api/userevent/{eventId}/{userId}")]
+        [HttpPost]
+        public IHttpActionResult AddUserToEvent(int eventId, int userId)
+        {
+            var repository = new UserEventRepository();
+
+            repository.addUserToEvent(eventId,userId);
+
+            return Ok();
+        }
+    }
+}
