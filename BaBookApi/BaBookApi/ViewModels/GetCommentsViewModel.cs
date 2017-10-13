f460ecce1f8d3888a9160e7067356294a42634eb
Vitalija Rudkevich <supah_girl@mail.ru>
2017-10-13 15:09:47 +0300

    NewEventViewModel, GetCommentsViewModel added

---
+++ BaBookApi/BaBookApi/ViewModels/GetCommentsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace BaBookApi.ViewModels
+{
+    public class GetCommentsViewModel
+    {
+        [JsonProperty("name")]
+        public int OwnerUser { get; set; }
+        [JsonProperty("comment")]
+        public string Text { get; set; }
+    }
+}
---
+++ BaBookApi/BaBookApi/ViewModels/NewEventViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace BaBookApi.ViewModels
+{
+    public class NewEventViewModel
+    {
+        [JsonProperty("groupId")]
+        public int GroupId { get; set; }
+        [JsonProperty("userId")]
+        public int OwnerId { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
+        [JsonProperty("date")]
+        public DateTime DateOfOccurance { get; set; }
+        [JsonProperty("comment")]
+        public string Description { get; set; }
+        [JsonProperty("location")]
+        public string Location { get; set; }
+    }
+}
