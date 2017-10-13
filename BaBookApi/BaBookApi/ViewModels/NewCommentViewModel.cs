698bbcceb9349d47e36fa14e2743906571517ffe
Andzej Korovacki <Andrzej.korovacki@gmail.com>
2017-10-13 15:21:23 +0300

    AttendanceVm, NewCommentVm

---
+++ BaBookApi/BaBookApi/ViewModels/NewCommentViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace BaBookApi.ViewModels
+{
+    public class NewCommentViewModel
+    {
+        [JsonProperty("userId")]
+        public int UserId { get; set; }
+
+        [JsonProperty("commentText")]
+        public string CommentText { get; set; }
+    }
+}
