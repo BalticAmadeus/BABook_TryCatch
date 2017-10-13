46eaa20587ffa513041d8e0bdb475ab649e9c6e0
Gabrielius Kazakevičius <gkazakevic@gmail.com>
2017-10-13 14:55:19 +0300

    Mapping reset

--- BaBookApi/BaBookApi/Mapping/ViewModelToDomainMapping.cs
+++ BaBookApi/BaBookApi/Mapping/ViewModelToDomainMapping.cs
@@ -11,24 +11,6 @@ namespace BaBookApi.Mapping
 {
     public static class ViewModelToDomainMapping
     {
-        public static Event MapEvent(CreateEventViewModel model)
-        {
-            var currentEvent = new Event
-            {
-                EventId = model.EventId,
-                DateOfOccurance = model.DateOfOccurance,
-                Location = model.Location,
-                Title = model.Title,
-                Description = model.Description
-            };
-
-            currentEvent.Attendances = new List<UserEventAttendance>();
-            currentEvent.Comments = new List<Comment>();
-
-            currentEvent.OfGroup = new Group();
-            currentEvent.OwnerUser = new User();
-
-            return currentEvent;
-        }
+       
     }
 }
