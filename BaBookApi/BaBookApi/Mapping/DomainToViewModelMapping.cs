0d3369f19d9d94915aa868d7e234c3fccb054e22
Andzej Korovacki <Andrzej.korovacki@gmail.com>
2017-10-13 15:48:57 +0300

    Added mapping of AttendanceVm and CommentVm

--- BaBookApi/BaBookApi/Mapping/DomainToViewModelMapping.cs
+++ BaBookApi/BaBookApi/Mapping/DomainToViewModelMapping.cs
@@ -9,6 +9,27 @@ namespace BaBookApi.Mapping
 {
     public static class DomainToViewModelMapping
     {
-        
+        public static NewCommentViewModel NewCommentModelToViewModel(Comment comment)
+        {
+            var commentViewModel = new NewCommentViewModel
+            {
+                UserId = comment.OwnerUser.UserId,
+                CommentText = comment.CommentText
+            };
+
+            return commentViewModel;
+        }
+
+        public static AttendanceViewModel AttendanceModelToViewModel(UserEventAttendance attendance)
+        {
+            var attendanceViewModel = new AttendanceViewModel
+            {
+                UserId = attendance.User.UserId,
+                EventId = attendance.Event.EventId,
+                Status = attendance.Response
+            };
+
+            return attendanceViewModel;
+        }
     }
 }
